6f054846158a6770d74be080d25a8eeab965d643
dejankaranovicprivate <dejan301186@gmail.com>
2021-02-25 13:59:15 +0100

    Logic for writer service is implemented.

--- RuleEngine/Services/WriterService.cs
+++ RuleEngine/Services/WriterService.cs
@@ -1,6 +1,8 @@
 ﻿using RuleEngine.Interfaces;
 using RuleEngine.Model;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace RuleEngine.Services
 {
@@ -8,7 +10,13 @@ namespace RuleEngine.Services
     {
         public void WriteDiagnosis(List<LiveEvent> liveEvents, List<Rule> rules)
         {
-
+            foreach (var rule in from liveEvent in liveEvents
+                                 from rule in rules
+                                 where rule.TurbineIds.Contains(liveEvent.TurbineId)
+                                 select rule)
+            {
+                Console.WriteLine(rule.Diagnosis);
+            }
         }
     }
 }
